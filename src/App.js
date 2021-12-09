fd7d7a3ba740e2fab5bec5fff915834ab696a37e
19arman99 <87122283+19arman99@users.noreply.github.com>
2021-12-09 21:22:43 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Main from '../src/Component/Main'
+import Footer from '../src/Component/Footer'
+
+function App() {
+  return (
+    <div className="App-body">
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
